8ec8cc4ae4c0c77b251b61859ef27cfb30dce532
cakewhip <phan.kavin@gmail.com>
2020-08-06 15:26:50 -0400

    Fix parity issue between block tab provider and block tab

--- src/main/java/com/kqp/inventorytabs/tabs/provider/BlockTabProvider.java
+++ src/main/java/com/kqp/inventorytabs/tabs/provider/BlockTabProvider.java
@@ -3,6 +3,7 @@ package com.kqp.inventorytabs.tabs.provider;
 import com.kqp.inventorytabs.tabs.tab.Tab;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -24,10 +25,15 @@ public abstract class BlockTabProvider implements TabProvider {
                     BlockPos blockPos = player.getBlockPos().add(x, y, z);
 
                     if (matches(world, blockPos)) {
-                        Tab tab = createTab(world, blockPos);
+                        Vec3d playerHead = player.getPos().add(0D, player.getEyeHeight(player.getPose()), 0D);
+                        Vec3d blockVec = new Vec3d(blockPos.getX() + 0.5D, blockPos.getY() + 0.5D, blockPos.getZ() + 0.5D);
 
-                        if (!tabs.contains(tab)) {
-                            tabs.add(tab);
+                        if (blockVec.subtract(playerHead).lengthSquared() <= SEARCH_DISTANCE * SEARCH_DISTANCE) {
+                            Tab tab = createTab(world, blockPos);
+
+                            if (!tabs.contains(tab)) {
+                                tabs.add(tab);
+                            }
                         }
                     }
                 }
